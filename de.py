57044a5b18cd8d92b9b46fb477ccba64f0831748
goofw <59505666+goofw@users.noreply.github.com>
2025-03-22 18:22:18 +0800

    Update de.py

--- de.py
+++ de.py
@@ -24,6 +24,9 @@ while True:
             continue
         if server[idx] in [ "'", "\"", "`", "/" ]:
             if open_quote == server[idx]:
+                # F U for not escaping / in regex
+                # search DAYS = [ 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ] for the stupid code
+                if server[idx: idx + 3] in [ "/?]", "/=?", "/;?" ]: continue
                 open_quote = None
                 if module_id == 359000:
                     print('###', left, STOP, server[open_quote_idx: min(idx + 1, open_quote_idx + 200)])
@@ -32,8 +35,8 @@ while True:
             elif not open_quote:
                 if server[idx] == "/":
                     jdx = idx - 1
-                    while jdx >= 0 and server[jdx] == " ": jdx -= 1
-                    if server[jdx] not in [ "(", "[", "{", "=", "!", ":" ]: continue
+                    while jdx >= 0 and server[jdx] in [ " ", "\t", "\n" ]: jdx -= 1
+                    if server[jdx] not in [ "(", "[", "{", ",", "=", ":", "!", "&", "|" ]: continue
                 open_quote = server[idx]
                 open_quote_idx = idx
                 escape = False
